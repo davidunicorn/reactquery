8ae5b488fa7c54184543e4938ee7d90f61f99ce2
David Gatica <david.gatica@unosquare.com>
2023-11-24 08:45:16 -0600

    Improve HTTP Request UX, Error Handling, Loading, Update State

--- 01-app-random/src/App.tsx
+++ 01-app-random/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useReducer } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const getRandomNumberFromAPI = async ():Promise<number> => {
+  const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
+  const numberString = await res.text();
+ //throw new Error('You got an Error');
+  return +numberString;
+}
+export const App = () => {
+  const [number, setNumber] = useState<number>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] =  useState<string>();
+  //[state, dispatch]
+  const [key, forceRefetch] = useReducer((x)=> x + 1 ,0);
+
+  useEffect(()=>{
+    setIsLoading(true);
+    getRandomNumberFromAPI()
+    .then( num => setNumber(num))
+    .catch(error => setError(error.message));
+  }, [key]);
+
+  useEffect(()=>{
+    if (number) setIsLoading(false);
+  }, [number]);
+
+  useEffect(()=>{
+    if (error) setIsLoading(false);
+
+  }, [error]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App App-header">
+      {isLoading && ( <h2>Cargando...</h2>)} 
+      {!error && (<h2>Randon Number: { number }</h2>) }
+      {!isLoading && error && (<h3>{error}</h3>)}
+      <button onClick={forceRefetch} disabled={isLoading}>
+        {isLoading ? '...' : 'Nuevo Numero' }
+      </button>
+    </div>
   )
 }
 
-export default App
